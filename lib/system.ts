652870c7f8bfa523bfe8a8186b55fe0cb53d6470
pichead <61868523+pichead@users.noreply.github.com>
2025-02-08 04:01:14 +0700

    update check plateform before exec

--- lib/system.ts
+++ lib/system.ts
@@ -5,43 +5,51 @@ import { promisify } from "util";
 const execAsync = promisify(exec);
 
 function getCpuUsage() {
-  const cpus = os.cpus();
-  return cpus.map((cpu) => {
-    const total = Object.values(cpu.times).reduce((acc, tv) => acc + tv, 0);
-    const usage = 100 - (100 * cpu.times.idle) / total;
-    return usage.toFixed(1);
-  });
+    const cpus = os.cpus();
+    return cpus.map((cpu) => {
+        const total = Object.values(cpu.times).reduce((acc, tv) => acc + tv, 0);
+        const usage = 100 - (100 * cpu.times.idle) / total;
+        return usage.toFixed(1);
+    });
 }
 
 async function getCpuTemp() {
-  const { stdout } = await execAsync("vcgencmd measure_temp");
-  // in celsius! OBVIOUSLY!
-  return parseFloat(stdout.replace("temp=", "").replace("'C", ""));
+    if (process.platform !== "linux") {
+        return 0; // หรือค่า default อะไรก็ได้ เช่น 0
+    }
+
+    try {
+        const { stdout } = await execAsync("vcgencmd measure_temp");
+        return parseFloat(stdout.replace("temp=", "").replace("'C", ""));
+    } catch (error) {
+        console.error("Failed to get CPU temperature:", error);
+        return 0;
+    }
 }
 
 function bytesToGB(bytes: number) {
-  return (bytes / (1024 * 1024 * 1024)).toFixed(2);
+    return (bytes / (1024 * 1024 * 1024)).toFixed(2);
 }
 
 export async function getSystemDetails() {
-  // Get CPU usage
-  const cpuUsage = getCpuUsage();
+    // Get CPU usage
+    const cpuUsage = getCpuUsage();
 
-  // Get memory info
-  const totalMem = os.totalmem();
-  const freeMem = os.freemem();
-  const usedMem = totalMem - freeMem;
- 
-  const cpuTemp = await getCpuTemp();
+    // Get memory info
+    const totalMem = os.totalmem();
+    const freeMem = os.freemem();
+    const usedMem = totalMem - freeMem;
 
-  return {
-    os,
-    cpuTemp,
-    cpuUsage,
-    memoryUsage: {
-      total: parseFloat(bytesToGB(totalMem)),
-      used: parseFloat(bytesToGB(usedMem)),
-      free: parseFloat(bytesToGB(freeMem)),
-    },
-  };
+    const cpuTemp = await getCpuTemp();
+
+    return {
+        os,
+        cpuTemp,
+        cpuUsage,
+        memoryUsage: {
+            total: parseFloat(bytesToGB(totalMem)),
+            used: parseFloat(bytesToGB(usedMem)),
+            free: parseFloat(bytesToGB(freeMem)),
+        },
+    };
 }
